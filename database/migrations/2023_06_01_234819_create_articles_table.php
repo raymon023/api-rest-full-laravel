0345a372d4da15a0c16c3197895c03ad7d07ae5a
raymon023 <raymonrodriguez023@gmail.com>
2023-06-01 20:08:29 -0400

    add fields unique for slug

--- database/migrations/2023_06_01_234819_create_articles_table.php
+++ database/migrations/2023_06_01_234819_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('content');
 
             $table->foreignId('user_id')->constrained();
